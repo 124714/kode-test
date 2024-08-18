67794b82e1da846bd1d7a8516f4ac6e9ff369d85
Anatoliy <tolikantonov@icloud.com>
2024-08-18 17:53:25 +0300

    add[common.data.cache]: Добавил классы для кэширования

---
+++ app/src/main/java/com/antoan/kodetest/common/data/cache/daos/EmployeeDao.kt
@@ -0,0 +1,19 @@
+package com.antoan.kodetest.common.data.cache.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.antoan.kodetest.common.data.cache.model.CachedEmployee
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDao {
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  fun insertEmployee(employees: List<CachedEmployee>)
+
+  @Query("SELECT * FROM employee")
+  fun getAllEmployees(): Flow<List<CachedEmployee>>
+
+}
