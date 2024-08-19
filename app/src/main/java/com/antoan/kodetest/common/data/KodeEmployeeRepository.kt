09552adf8c2d47ef498c81aa568c600b21d63bda
Anatoliy <tolikantonov@icloud.com>
2024-08-19 16:23:40 +0300

    add[data.repository, domain.repository]: Определил границу между слоями

---
+++ app/src/main/java/com/antoan/kodetest/common/data/KodeEmployeeRepository.kt
@@ -0,0 +1,45 @@
+package com.antoan.kodetest.common.data
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.antoan.kodetest.common.data.api.KodeApi
+import com.antoan.kodetest.common.data.api.model.mappers.ApiEmployeeMapper
+import com.antoan.kodetest.common.data.cache.model.Cache
+import com.antoan.kodetest.common.data.cache.model.CachedEmployee
+import com.antoan.kodetest.common.domain.model.Employee
+import com.antoan.kodetest.common.domain.repository.EmployeeRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+/**
+ * Определяет границу (domain | data), так как реализует интерфейс domain
+ */
+
+@RequiresApi(Build.VERSION_CODES.O)
+class KodeEmployeeRepository @Inject constructor(
+  private val api: KodeApi,
+  private val cache: Cache,
+  private val apiEmployeeMapper: ApiEmployeeMapper
+) : EmployeeRepository {
+
+
+  override fun getEmployees(): Flow<List<Employee>> {
+    return cache
+      .getEmployees()
+      .distinctUntilChanged()
+      .map { employeeList -> employeeList.map(CachedEmployee::toDomain) }
+  }
+
+  override suspend fun requestEmployees(): List<Employee> {
+    val apiResponse = api.getAllUsers()
+    return apiResponse.items?.map { apiEmployeeMapper.mapToDomain(it) }.orEmpty()
+  }
+
+  override fun storeEmployees(employees: List<Employee>) {
+    cache.storeEmployees(
+        employees.map(CachedEmployee::fromDomain)
+      )
+  }
+}
--- app/src/main/java/com/antoan/kodetest/common/domain/repository/EmployeeRepository.kt
+++ app/src/main/java/com/antoan/kodetest/common/domain/repository/EmployeeRepository.kt
@@ -1,4 +1,10 @@
 package com.antoan.kodetest.common.domain.repository
 
+import com.antoan.kodetest.common.domain.model.Employee
+import kotlinx.coroutines.flow.Flow
+
 interface EmployeeRepository {
+  fun getEmployees(): Flow<List<Employee>>
+  suspend fun requestEmployees(): List<Employee>
+  fun storeEmployees(employees: List<Employee>)
 }
